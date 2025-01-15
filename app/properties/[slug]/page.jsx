c32c6b9d6eb0e73e5d14dce0feb891db3cb4a87b
Ginjak <89803801+Ginjak@users.noreply.github.com>
2025-01-15 20:51:40 +0000

    aas

--- app/properties/[slug]/page.jsx
+++ app/properties/[slug]/page.jsx
@@ -113,13 +113,6 @@ export default async function PropertyPage({ params }) {
             latitude={data?.latitude}
           />
           <SinglePostMap lat={data?.latitude} lng={data?.longitude} />
-          {/* <div className="relative h-[500px]">
-            <div className="relative h-full">
-              <div className="absolute inset-0 flex items-center justify-center bg-property-txt-700/5">
-                <div className="w-10 h-10 border-4 border-property-acc-100 border-t-transparent rounded-full animate-spin"></div>
-              </div>
-            </div>
-          </div> */}
         </div>
 
         {/* Agent Information (only visible on larger screens) */}
