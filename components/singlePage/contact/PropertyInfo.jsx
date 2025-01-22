d9e9f2782d5024a96b8dbe4942873fef6f7fe299
Ginjak <89803801+Ginjak@users.noreply.github.com>
2025-01-22 21:37:59 +0000

    preload image in contact inner

--- components/singlePage/contact/PropertyInfo.jsx
+++ components/singlePage/contact/PropertyInfo.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { IoIosBed } from "react-icons/io";
 import { PiArmchairFill } from "react-icons/pi";
 import { TbBathFilled } from "react-icons/tb";
+import Head from "next/head";
 
 export default function PropertyInfo({
   image,
@@ -12,71 +13,78 @@ export default function PropertyInfo({
   companyLogoAlt,
 }) {
   return (
-    <div className="rounded-xl flex flex-col sm:flex-row lg:flex-col bg-property-txt-700/5 border-[1px] border-property-txt-700/10 sm:shadow-md">
-      <ImageWithSpinner
-        src={image}
-        alt={alt}
-        width={"640"}
-        height={"400"}
-        className={
-          "rounded-t-xl sm:rounded-none sm:rounded-l-xl lg:rounded-none lg:rounded-t-xl  sm:max-w-80 lg:max-w-full object-cover lg:object-contain"
-        }
-        priority={"priority"}
-        spinnerClassName={
-          "rounded-t-xl sm:rounded-none sm:rounded-l-xl lg:rounded-none lg:rounded-t-xl"
-        }
-      />
+    <>
+      <Head>
+        /* Preload the first image to improve LCP */
+        <link rel="preload" href={image} as="image" />
+      </Head>
 
-      <div className="p-4 flex flex-col gap-2 w-full">
-        <p className="text-property-txt-700 text-xl font-bold tracking-wide">
-          £
-          {data?.original_price &&
-            new Intl.NumberFormat().format(data.original_price)}
-        </p>
-        <p className="text-property-txt-700 text-lg font-medium tracking-wide">
-          {data?.bedrooms} bed {data?.property_type} for sale{" "}
-        </p>
-        <p className="font-normal text-property-txt-700/60">
-          {data?.address_line}, {data?.borough ? data?.borough : data?.city}{" "}
-          {data?.postcode?.split(" ")[0]}
-        </p>
-        <div className="flex gap-2">
-          <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
-            <IoIosBed className="text-property-acc-100" /> {data?.bedrooms}
+      <div className="rounded-xl flex flex-col sm:flex-row lg:flex-col bg-property-txt-700/5 border-[1px] border-property-txt-700/10 sm:shadow-md">
+        <ImageWithSpinner
+          src={image}
+          alt={alt}
+          width={"640"}
+          height={"400"}
+          className={
+            "rounded-t-xl sm:rounded-none sm:rounded-l-xl lg:rounded-none lg:rounded-t-xl  sm:max-w-80 lg:max-w-full object-cover lg:object-contain"
+          }
+          priority={"priority"}
+          spinnerClassName={
+            "rounded-t-xl sm:rounded-none sm:rounded-l-xl lg:rounded-none lg:rounded-t-xl"
+          }
+        />
+
+        <div className="p-4 flex flex-col gap-2 w-full">
+          <p className="text-property-txt-700 text-xl font-bold tracking-wide">
+            £
+            {data?.original_price &&
+              new Intl.NumberFormat().format(data.original_price)}
           </p>
-          <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
-            <TbBathFilled className="text-property-acc-100" />
-            {data?.bathrooms}
+          <p className="text-property-txt-700 text-lg font-medium tracking-wide">
+            {data?.bedrooms} bed {data?.property_type} for sale{" "}
           </p>
-          {data?.receptions && (
+          <p className="font-normal text-property-txt-700/60">
+            {data?.address_line}, {data?.borough ? data?.borough : data?.city}{" "}
+            {data?.postcode?.split(" ")[0]}
+          </p>
+          <div className="flex gap-2">
             <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
-              <PiArmchairFill className="text-property-acc-100" />
-              {data?.receptions}
+              <IoIosBed className="text-property-acc-100" /> {data?.bedrooms}
             </p>
-          )}
-        </div>
-        <div className="mt-4 flex justify-between items-center">
-          <div className="flex flex-col">
-            {data?.team_member?.team_member_name && (
-              <>
-                <p className="text-property-txt-700/60 text-xs">Agent</p>
-                <p className="text-property-txt-700 font-medium">
-                  {data?.team_member?.team_member_name}{" "}
-                  {data?.team_member?.team_member_surname}
-                </p>
-              </>
+            <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
+              <TbBathFilled className="text-property-acc-100" />
+              {data?.bathrooms}
+            </p>
+            {data?.receptions && (
+              <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
+                <PiArmchairFill className="text-property-acc-100" />
+                {data?.receptions}
+              </p>
             )}
           </div>
-          <ImageWithSpinner
-            src={companyLogo}
-            alt={companyLogoAlt}
-            width={"100"}
-            height={"400"}
-            loading={"lazy"}
-            spinnerClassName={"z-10 rounded-xl max-h-14"}
-          />
+          <div className="mt-4 flex justify-between items-center">
+            <div className="flex flex-col">
+              {data?.team_member?.team_member_name && (
+                <>
+                  <p className="text-property-txt-700/60 text-xs">Agent</p>
+                  <p className="text-property-txt-700 font-medium">
+                    {data?.team_member?.team_member_name}{" "}
+                    {data?.team_member?.team_member_surname}
+                  </p>
+                </>
+              )}
+            </div>
+            <ImageWithSpinner
+              src={companyLogo}
+              alt={companyLogoAlt}
+              width={"100"}
+              height={"400"}
+              loading={"lazy"}
+              spinnerClassName={"z-10 rounded-xl max-h-14"}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
