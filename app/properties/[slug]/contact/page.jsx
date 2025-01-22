45c7752cb34175b66b3eb80ad81652f3ba288903
Ginjak <89803801+Ginjak@users.noreply.github.com>
2025-01-22 17:41:20 +0000

    added static params to inner contact

--- app/properties/[slug]/contact/page.jsx
+++ app/properties/[slug]/contact/page.jsx
@@ -1,32 +1,42 @@
 import AgentForm from "@/components/singlePage/contact/AgentForm";
-
 import PropertyInfo from "@/components/singlePage/contact/PropertyInfo";
-import { fetchCptShortSinglePost, fetchImageData } from "@/lib/api";
+import {
+  fetchCptShortSinglePost,
+  fetchImageData,
+  fetchAllCptPosts,
+} from "@/lib/api";
 
-export default async function page({ params }) {
+// Generate static params for all slugs
+export async function generateStaticParams() {
+  const posts = await fetchAllCptPosts({ slug: "properties" });
+  return posts.posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+// Page Component
+export default async function ContactPage({ params }) {
   const { slug } = await params;
-  console.log("slug", slug);
+
   // Fetch single property data
   const data = await fetchCptShortSinglePost("properties-short", slug);
-  const url = `${process.env.NEXT_MAIN_DOMAIN_NAME}/${data.post_type}/${slug}`;
-  console.log("base url", url);
 
+  if (!data) {
+    // Return 404 if no data is found
+    notFound();
+  }
+
+  const url = `${process.env.NEXT_MAIN_DOMAIN_NAME}/${data.post_type}/${slug}`;
+
+  // Fetch related images in parallel
   const [featuredImage, companyLogo] = await Promise.all([
     fetchImageData([data?.featured_image]),
     fetchImageData([data?.team_member?.team_member_company_logo]),
   ]);
 
-  console.log("featured", featuredImage);
-  console.log("compnay", companyLogo);
-
-  // If no data is found, return a fallback page
-  if (!data) {
-    // Change this to 404 component
-    return <p>No data found for this property.</p>;
-  }
   return (
     <div className="max-w-5xl mx-auto px-3">
-      <h2 className="font-medium  pb-3 md:pb-5 text-xl md:text-3xl tracking-wide text-property-txt-700 py-4">
+      <h2 className="font-medium pb-3 md:pb-5 text-xl md:text-3xl tracking-wide text-property-txt-700 py-4">
         Email agent
       </h2>
       <div className="flex flex-col-reverse lg:flex-row">
@@ -35,10 +45,10 @@ export default async function page({ params }) {
         </div>
         <div className="w-full lg:block lg:w-1/3 lg:ps-4 overflow-y-auto mt-0 mb-6 lg:mb-0">
           <PropertyInfo
-            image={featuredImage[0].guid?.rendered}
-            alt={featuredImage[0].alt_text}
-            companyLogo={companyLogo[0].guid?.rendered}
-            companyLogoAlt={companyLogo[0].alt_text}
+            image={featuredImage?.[0]?.guid?.rendered || ""}
+            alt={featuredImage?.[0]?.alt_text || ""}
+            companyLogo={companyLogo?.[0]?.guid?.rendered || ""}
+            companyLogoAlt={companyLogo?.[0]?.alt_text || ""}
             data={data}
           />
         </div>
