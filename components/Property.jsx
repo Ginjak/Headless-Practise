0cd93cab59f7c0846223e1f33f334c22c2e569e5
Ginjak <89803801+Ginjak@users.noreply.github.com>
2025-01-24 12:09:39 +0000

    archive properties cards completed with responsivness

--- components/Property.jsx
+++ components/Property.jsx
@@ -44,7 +44,7 @@ export default function Property({ property }) {
   return (
     <Link
       href={`/${property?.post_type}/${property?.slug}`}
-      className="rounded-xl xs:border-[1px] xs:border-property-txt-700/10 xs:shadow-md"
+      className="rounded-xl border-[1px] border-property-txt-700/10 shadow-md"
     >
       <div className="flex flex-col h-full">
         {propertyImg && (
@@ -61,35 +61,35 @@ export default function Property({ property }) {
             className={"rounded-t-lg"}
           />
         )}
-        <div className="p-4 flex flex-col justify-between grow">
+        <div className="p-4 xs:p-2 sm:p-4 flex flex-col justify-between grow">
           <div>
-            <div className="flex justify-between mb-2">
-              <p className="text-property-txt-700 text-lg font-bold tracking-wide">
+            <div className="flex xs:flex-col sm:flex-row justify-between mb-2">
+              <p className="text-property-txt-700 text-lg xs:text-base sm:text-lg font-bold tracking-wide">
                 £
                 {property?.original_price &&
                   new Intl.NumberFormat().format(property?.original_price)}
               </p>
-              <p className="text-property-txt-700">
+              <p className="text-property-txt-700 xs:text-sm sm:text-base">
                 {property?.property_type?.charAt(0).toUpperCase() +
                   property?.property_type?.slice(1)}
               </p>
             </div>
-            <p className="text-property-txt-700 tracking-wide mb-4">
+            <p className="text-property-txt-700 tracking-wide mb-4 xs:text-sm sm:text-base">
               {property?.address_line},{" "}
               {property?.borough ? property?.borough : property?.city}{" "}
               {property?.postcode?.split(" ")[0]}
             </p>
-            <div className="flex gap-2">
-              <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
+            <div className="flex gap-2 xs:flex-col sm:flex-row">
+              <p className="flex items-center gap-1 text-property-txt-700  pe-2 xs:pe-0 sm:pe-2 border-r-[1px] xs:border-r-[0px] sm:border-r-[1px] border-property-txt-700/20 last:border-0">
                 <IoIosBed className="text-property-acc-100" />{" "}
                 <span className="text-sm">{property?.bedrooms}</span>
               </p>
-              <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
+              <p className="flex items-center gap-1 text-property-txt-700  pe-2 xs:pe-0 sm:pe-2 border-r-[1px] xs:border-r-[0px] sm:border-r-[1px] border-property-txt-700/20 last:border-0">
                 <TbBathFilled className="text-property-acc-100" />
                 <span className="text-sm">{property?.bathrooms}</span>
               </p>
               {property?.receptions && (
-                <p className="flex items-center gap-1 text-property-txt-700  pe-2 border-r-[1px] border-property-txt-700/20 last:border-0">
+                <p className="flex items-center gap-1 text-property-txt-700  pe-2 xs:pe-0 sm:pe-2 border-r-[1px] xs:border-r-[0px] sm:border-r-[1px] border-property-txt-700/20 last:border-0">
                   <RiCustomSize className="text-property-acc-100" />
                   <span className="text-sm">
                     {property?.size_in_sq_ft &&
@@ -103,37 +103,38 @@ export default function Property({ property }) {
           {property?.team_member_id && (
             <div>
               <SinglePostDivider className={"my-3"} />
-              <div className="flex items-center gap-4">
-                <ImageWithSpinner
-                  src={
-                    agentImg?.media_details?.sizes?.thumbnail?.source_url ||
-                    agentImg?.guid?.rendered ||
-                    "/agent_placeholder.webp"
-                  }
-                  alt={agentImg?.alt_text || "Agent avart image"}
-                  width={40}
-                  height={40}
-                  className={"rounded-full"}
-                />
-                <div className="flex justify-between w-full">
+              <div className="flex xs:flex-col sm:flex-row items-between gap-4">
+                <div className="flex justify-start xs:justify-between sm:justify-start gap-3 items-center w-full">
+                  <ImageWithSpinner
+                    src={
+                      agentImg?.media_details?.sizes?.thumbnail?.source_url ||
+                      agentImg?.guid?.rendered ||
+                      "/agent_placeholder.webp"
+                    }
+                    alt={agentImg?.alt_text || "Agent avart image"}
+                    width={40}
+                    height={40}
+                    className={"rounded-full"}
+                  />
+
                   <p className="flex flex-col leading-none text-sm text-property-txt-700">
                     {property?.team_member_name}{" "}
                     <span>{property?.team_member_surname}</span>
                   </p>
-                  <div className="flex items-center gap-3">
-                    <Link
-                      href={`tel:${property?.team_member_phone}`}
-                      className="p-2 bg-property-acc-100 hover:bg-property-acc-300 rounded-lg text-white transition-colors duration-300"
-                    >
-                      <FaPhone />
-                    </Link>
-                    <Link
-                      href={`${property?.post_type}/${property?.slug}/contact`}
-                      className="p-2 bg-property-acc-100 hover:bg-property-acc-300 rounded-lg text-white transition-colors duration-300"
-                    >
-                      <BsEnvelopeAt />
-                    </Link>
-                  </div>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Link
+                    href={`tel:${property?.team_member_phone}`}
+                    className="p-2 bg-property-acc-100 hover:bg-property-acc-300 rounded-lg text-white transition-colors duration-300 w-auto flex justify-center xs:w-full sm:width-auto text-center "
+                  >
+                    <FaPhone />
+                  </Link>
+                  <Link
+                    href={`${property?.post_type}/${property?.slug}/contact`}
+                    className="p-2 bg-property-acc-100 hover:bg-property-acc-300 rounded-lg text-white transition-colors duration-300 w-auto flex justify-center xs:w-full sm:width-auto text-center"
+                  >
+                    <BsEnvelopeAt />
+                  </Link>
                 </div>
               </div>
             </div>
